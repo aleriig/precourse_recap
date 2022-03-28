753ea949288543bb0ab2b152e53a4ab647834807
Alejo Rios <alejoriosiglesias@gmail.com>
2022-03-28 17:10:39 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,20 @@
+my_name = "alejo"
+my_age = 36
+
+print("My name is " + my_name + "and I'm " + my_age)
+
+furniture_1 = "couch"
+price_furniture_1= 250
+
+furniture_2 = "table"
+price_furniture_2 = 146
+
+furniture_3 = "lamp"
+price_furniture_3 = 87
+
+#VAT 20%
+tax_price = .2
+
+total_price = price_furniture_1 + price_furniture_2 + price_furniture_3
+total_price_tax = total_price * tax_price
+print(total_price_tax)
